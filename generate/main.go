1758b9df8a31cbcba781f951f5833400984f1897
Frank Chiarulli Jr. <frank@frankchiarulli.com>
2025-10-09 21:37:36 -0400

    cleanup error handling

--- generate/main.go
+++ generate/main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"os"
 	"text/template"
-	"context"
 	"time"
 
-	"github.com/mmcdole/gofeed"
 	_ "embed"
+
+	"github.com/mmcdole/gofeed"
 )
 
 //go:embed README.md.tmpl
 var readmeTemplate string
 
-type ReadmeData struct{
+type ReadmeData struct {
 	LatestBlogPost *BlogPost
 }
 
@@ -38,8 +39,13 @@ func main() {
 	}
 	defer outFile.Close()
 
+	latestBlogPost, err := getLatestBlogPost()
+	if err != nil {
+		panic(err)
+	}
+
 	readmeData := ReadmeData{
-		LatestBlogPost: getLatestBlogPost(),
+		LatestBlogPost: latestBlogPost,
 	}
 
 	err = renderReadmeToWriter(&readmeData, outFile)
@@ -57,25 +63,23 @@ func renderReadmeToWriter(readmeData *ReadmeData, writer io.Writer) error {
 	return template.ExecuteTemplate(writer, "", readmeData)
 }
 
-func getLatestBlogPost() *BlogPost {
+func getLatestBlogPost() (*BlogPost, error) {
 	fp := gofeed.NewParser()
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	
+
 	feed, err := fp.ParseURLWithContext("https://frankchiarulli.com/blog/rss.xml", ctx)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Warning: Failed to fetch blog RSS feed: %v\n", err)
-		return nil
+		return nil, fmt.Errorf("error parsing RSS feed: %w", err)
 	}
-	
+
 	if len(feed.Items) == 0 {
-		fmt.Fprintf(os.Stderr, "Warning: No blog posts found in RSS feed\n")
-		return nil
+		return nil, fmt.Errorf("no blog posts found")
 	}
-	
+
 	latest := feed.Items[0]
 	return &BlogPost{
 		Title: latest.Title,
 		URL:   latest.Link,
-	}
+	}, nil
 }
